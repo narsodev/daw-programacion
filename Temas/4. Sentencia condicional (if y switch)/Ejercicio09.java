a43b1a38639421bad5bfe08a6ac3bcee064ca644
narsodev <narsodev@gmail.com>
2021-10-13 12:35:32 +0200

    fix exercise

--- Temas/4. Sentencia condicional (if y switch)/Ejercicio09.java
+++ Temas/4. Sentencia condicional (if y switch)/Ejercicio09.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Ejercicio09 {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
-    double a;
+    double a, b, x1;
 
     System.out.println("Este programa resuelve ecuaciones del tipo: ax2 + bx + c = 0");
 
     System.out.print("a = ");
     a = s.nextDouble();
 
-    if (a != 0) {
-      double b, c, valorIntermedio, x1, x2;
+    System.out.print("b = ");
+    b = s.nextDouble();
 
-      System.out.print("b = ");
-      b = s.nextDouble();
+    if (a != 0) {
+      double c, valorIntermedio, x2;
   
       System.out.print("c = ");
       c = s.nextDouble();
@@ -29,9 +29,18 @@ public class Ejercicio09 {
         x1 = (-b + valorIntermedio) / (2 * a);
         x2 = (-b - valorIntermedio) / (2 * a);
   
-        System.out.printf("x1 = %.2f\nx2 = %.2f", x1, x2);
+        System.out.printf("\nx1 = %.2f\nx2 = %.2f", x1, x2);
       }
-    } else System.out.println("Esta ecuación no tiene solución.");
+    } else if (b != 0) {
+      double c;
+  
+      System.out.print("c = ");
+      c = s.nextDouble();
+
+      x1 = -c / b;
+
+      System.out.printf("\nx = %.2f", x1);
+    } else System.out.println("\nEsta ecuación no tiene solución.");
 
     s.close();
   }
