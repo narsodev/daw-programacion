0bc9b1baa457471e98b06ec4ae03767ed1f0e5aa
narsodev <narsodev@gmail.com>
2021-10-29 14:11:09 +0200

    Refactor exercise

--- Temas/5. Bucles/Ejercicio25.java
+++ Temas/5. Bucles/Ejercicio25.java
@@ -1,30 +1,18 @@
 /* Realiza un programa que pida un número por teclado y
 que luego muestre ese número al revés. */
-import java.util.Scanner;
-
 public class Ejercicio25 {
   public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
     int num;
     int numAlReves = 0;
-    int nCifras = 0;
-    int cifra;
 
     System.out.print("Introduzca el número: ");
-    num = s.nextInt();
-
-    s.close();
+    num = Integer.parseInt(System.console().readLine());
 
     for (int i = num; i > 0; i /= 10) {
-      nCifras++;
+      numAlReves *= 10;
+      numAlReves += i % 10;
     }
 
-    for (int i = nCifras - 1; i >= 0; i--) {
-      cifra = num / (int) Math.pow(10, i);
-      numAlReves += cifra * (int) Math.pow(10, nCifras - i - 1);
-      num -= cifra * (int) Math.pow(10, i);
-    }
-
-    System.out.println(numAlReves);
+    System.out.printf("El número %d al revés es: %d\n", num, numAlReves);
   }  
 }
